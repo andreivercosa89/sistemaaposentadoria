665f979faf9513c07666d5b852eb6068c191a5e9
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-31 21:42:28 -0400

    Verificar se a pessoa tem direito a aposentadoria

---
+++ sistemaaposentadoria/Aposentadoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace sistemaaposentadoria
+{
+    public class Aposentadoria
+    {
+        public string Nome { set; get; }
+        public double Idade { set; get; }
+        public double anosTrabalhados { set; get; }
+
+        public string respostaAposentadoria(){
+            if (Idade >= 65 || anosTrabalhados >= 30)
+            {
+                return "apto";
+            }
+            else if (Idade >= 60 & anosTrabalhados >= 25){
+                return "apto";
+            }else{
+                return "inapto";
+            }
+        }
+
+    }
+
+}
---
+++ sistemaaposentadoria/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sistemaaposentadoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aposentadoria aposentadoria = new Aposentadoria();
+
+            Console.WriteLine("Infome o Nome: ");
+            aposentadoria.Nome = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("\nInfome sua Idade: ");
+            aposentadoria.Idade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInfome os anos Trabalhados: ");
+            aposentadoria.anosTrabalhados = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\n{0} esta {1} para se aposentar.", aposentadoria.Nome, aposentadoria.respostaAposentadoria());
+        }
+    }
+}
